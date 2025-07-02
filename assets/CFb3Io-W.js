a550bfbfce8d54848d1212c454f6561b5c81ca64
Rohit Asare <rohitasare7@gmail.com>
2025-07-02 09:51:03 +0530

    Updates

---
+++ assets/CFb3Io-W.js
@@ -0,0 +1,1 @@
+import{R as s}from"./-T6n4kH9.js";import{u}from"./Bjk_n-cB.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
